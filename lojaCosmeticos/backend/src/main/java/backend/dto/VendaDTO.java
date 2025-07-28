a1ac7d55d5abc703308be0a5293f4b72becde056
CamaraEduarda <eduarda.camara@academico.ifpb.edu.br>
2025-07-28 14:18:51 -0300

    Inicialização dos pacotes

---
+++ lojaCosmeticos/backend/src/main/java/backend/dto/VendaDTO.java
@@ -0,0 +1,8 @@
+package backend.dto;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class VendaDTO {
+    
+}
---
+++ lojaCosmeticos/backend/src/main/java/backend/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package backend.repository;
+
+public class ProdutoRepository {
+
+
+    
+}
