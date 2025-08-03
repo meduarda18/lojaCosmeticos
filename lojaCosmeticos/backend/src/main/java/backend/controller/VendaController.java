615cd25c9ea8ccf4246458225fae1e368271258b
Maria Eduarda Câmara <144015917+CamaraEduarda@users.noreply.github.com>
2025-08-03 19:32:57 +0000

    feat[controller]: Criação do controller da entidade Venda

--- lojaCosmeticos/backend/src/main/java/backend/controller/VendaController.java
+++ lojaCosmeticos/backend/src/main/java/backend/controller/VendaController.java
@@ -1,5 +1,41 @@
 package backend.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import backend.dto.VendaDTO;
+import backend.service.VendaService;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/vendas")
 public class VendaController {
+    private final VendaService vendaService;
+
+    public VendaController(VendaService vendaService){
+        this.vendaService = vendaService;
+    }
     
+    @PostMapping
+    public ResponseEntity<VendaDTO> cadastrarVendas(@RequestBody VendaDTO vendaDTO) {
+        try {
+            VendaDTO novaVenda = vendaService.salvarVenda(vendaDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(novaVenda);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VendaDTO>> buscarPorPeriodo(@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start, 
+    @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) {
+        List<VendaDTO> vendas = vendaService.listarVendasPorPeriodo(start, end);
+        return ResponseEntity.ok(vendas);
+    }
 }
