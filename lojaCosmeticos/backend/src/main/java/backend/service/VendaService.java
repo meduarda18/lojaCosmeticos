0a4dbd42496be1b0ba75e6dde6483ea511f1b459
Maria Eduarda Câmara <144015917+CamaraEduarda@users.noreply.github.com>
2025-08-03 19:08:47 +0000

    feat[service]: Criação do service e dto da entidade Venda

--- lojaCosmeticos/backend/src/main/java/backend/service/VendaService.java
+++ lojaCosmeticos/backend/src/main/java/backend/service/VendaService.java
@@ -1,5 +1,12 @@
 package backend.service;
 
-public class VendaService {
+import java.time.LocalDateTime;
+import java.util.List;
+import backend.dto.VendaDTO;
+
+public interface VendaService {
+
+    List<VendaDTO> listarVendasPorPeriodo(LocalDateTime start, LocalDateTime end);
+    VendaDTO salvarVenda(VendaDTO vendaDTO);
     
 }
